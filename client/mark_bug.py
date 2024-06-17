402721019ece9de1b82d9a57b555dcb824e4c52b
marts-dev <8363978+marts-dev@users.noreply.github.com>
2024-06-18 03:57:51 +0800

    Update mark_bug.py

--- client/mark_bug.py
+++ client/mark_bug.py
@@ -3,9 +3,9 @@ from ghapi.core import GhApi
 
 def make_comment():
   owner,repo = os.getenv('REPO').split('/')
-  print(owner, repo)
-  #api = GhApi(owner=owner, repo=repo, token=os.getenv('GITHUB_TOKEN'))
-  #api.issues.add_labels(issue_number=os.environ['NUMBER'], labels=['bug'])
+  #print(owner, repo)
+  api = GhApi(owner=owner, repo=repo, token=os.getenv('GITHUB_TOKEN'))
+  api.issues.add_labels(issue_number=os.environ['NUMBER'], labels=['bug'])
 
 if __name__ == '__main__':
-    print(f'The comparison report can found at: {make_comment()}')
+    make_comment()
