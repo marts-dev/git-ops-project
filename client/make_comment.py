d032985dc71f9ff5cf00647c407900980df63814
marts-dev <8363978+marts-dev@users.noreply.github.com>
2024-06-21 01:08:05 +0800

    Updated wandb library used

--- client/make_comment.py
+++ client/make_comment.py
@@ -7,6 +7,7 @@ assert os.getenv('PR_NUMBER'), 'PR number must exist'
 def make_comment():
   api = GhApi(owner="marts-dev", repo="git-ops-project")
   api.issues.create_comment(os.getenv('PR_NUMBER'), f"Hi! your comparison report is ready: {os.getenv('REPORT_URL')}'.")
+  return os.getenv('REPORT_URL')
 
 if __name__ == '__main__':
     print(f'The comparison report can found at: {make_comment()}')
